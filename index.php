6e1daeb3515180350d7b6bf34d86ef03d4ec5fe9
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-16 17:00:20 +0300

    index structure done

--- index.php
+++ index.php
@@ -45,8 +45,39 @@ else if($_GET['page'] == "delete_user"){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My portfolio of services</title>
+    <link href="css/about.css" rel="stylesheet">
+    <link href="css/navbar.css" rel="stylesheet">
+    <link href="css/items.css" rel="stylesheet">
+    <link href="css/login.css" rel="stylesheet">
+    <link href="css/cart.css" rel="stylesheet">
 </head>
 <body>
-    
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php?page=home">HOME</a></li>
+            <li><a href="index.php?page=items&category=0">ALL SERVICES</a>
+                <ul class ="dropdown">
+                    <li><a href="index.php?page=items&category=1"></a>WEB</li>
+                    <li><a href="index.php?page=items&category=2"></a>C</li>
+                    <li><a href="index.php?page=items&category=3"></a>DEVOPS</li>
+                    <li><a href="ndex.php?page=items&category=4"></a>CONSULTING</li>
+                </ul>
+            </li>
+                <?php
+                    if ($_SESSION['loggued_on_user'] == "") 
+                    {
+                        echo "<li> <a href=\"index.php?page=login\">Login</a></li>";
+                    }
+                    else{
+                        echo "<li> <a href=\"index.php?page=profile\">Profile</a></li>";
+                        echo "<li> <a href=\"index.php?page=logout\">Log Out</a></li>";
+                    }
+                ?>
+            <li><a href="index.php?page=cart">BASKET</a></li>
+        </ul>
+    </nav>
+    <div class="contents-under">
+            <?php include $page; ?>
+    </div> 
 </body>
 </html>
