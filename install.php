8d1b0c71d73df5da522a2f7542dd94f80d085ece
Teemu Hakala <thakala@c1r6p12.hive.fi>
2022-04-16 20:22:08 +0300

    install categories

--- install.php
+++ install.php
@@ -10,13 +10,12 @@
 	$csv_item_categories = "./db/item_categories.csv";
 
 	$db_table_items = "items";
+	$db_table_categories = "categories";
+	$db_table_item_categories = "item_categories";
 
 	$db_initial_connection = mysqli_connect($db_host, $db_user, $db_pass);
 	if (mysqli_connect_error())
 		exit ("mysqli_connect returned NULL");
-	// print_r($db_initial_connection);
-
-
 
 	$sql_create_db = "CREATE DATABASE IF NOT EXISTS $db_name";
 	mysqli_query($db_initial_connection, $sql_create_db) OR
@@ -30,7 +29,7 @@
 	$db_connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name) OR
 		exit ("mysqli_connect returned NULL" . mysqli_error($db_connection));
 
-	$sql_create_table = "CREATE TABLE IF NOT EXISTS $db_table_items(
+	$sql_create_table = "CREATE TABLE IF NOT EXISTS $db_table_items (
 		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		name VARCHAR(100),
 		price INT,
@@ -40,40 +39,48 @@
 	mysqli_query($db_connection, $sql_create_table) OR
 		exit ("error creating table: `$db_table_items`" . mysqli_error($db_connection));
 
-
-
 	$sql = "SELECT * FROM $db_name.$db_table_items";
-    $query = mysqli_query($db_connection, $sql);
-    $query_row = mysqli_fetch_array($query);
-	// $sql_check_table_exists = "SELECT *
-	// 	FROM information_schema.tables
-	// 	WHERE table_schema = '$db_name'
-	// 		AND table_name = '$db_table_items'
-	// 	LIMIT 1";
-	// $query_result = mysqli_query($db_connection, $sql_check_table_exists) OR
-	// 	exit("error checking whether table `$db_table_items` exists");
-	// var_dump($query_result);
-	// echo "<br>";
-	// $query_array = mysqli_fetch_array($query_result);
-	// var_dump($query_array);
-	// echo "<br>";
-	// if ($query_row === false)
-	var_dump($query_row);
+	$query = mysqli_query($db_connection, $sql);
+	$query_row = mysqli_fetch_array($query);
 	if ($query_row == 0)
 	{
 		$csv_file = fopen($csv_items, "r");
 		if (($csv_fields = fgetcsv($csv_file)) === false)
 			exit ("fgetcsv returned `false` for `$csv_file`");
-		//unset($csv_fields[0]);
 		$csv_fields_str = implode(", ", $csv_fields);
 		while (($tuple = fgetcsv($csv_file)) !== false)
 		{
-			//unset($tuple[0]);
 			$sql_insert_csv_row = "INSERT INTO $db_table_items ($csv_fields_str)
 			VALUES ('".implode("', '", $tuple)."')";
 			mysqli_query($db_connection, $sql_insert_csv_row) OR
 				exit ("error inserting `$sql_insert_csv_row` into table: `$db_table_items`" . mysqli_error($db_connection));
 		}
 	}
+
+	$sql_create_table = "CREATE TABLE IF NOT EXISTS $db_table_categories (
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(100)
+	)";
+	mysqli_query($db_connection, $sql_create_table) OR
+		exit ("error creating table: `$db_table_categories`" . mysqli_error($db_connection));
+
+	$sql = "SELECT * FROM $db_name.$db_table_categories";
+	$query = mysqli_query($db_connection, $sql);
+	$query_row = mysqli_fetch_array($query);
+	if ($query_row == 0)
+	{
+		$csv_file = fopen($csv_categories, "r");
+		if (($csv_fields = fgetcsv($csv_file)) === false)
+			exit ("fgetcsv returned `false` for `$csv_file`");
+		$csv_fields_str = implode(", ", $csv_fields);
+		while (($tuple = fgetcsv($csv_file)) !== false)
+		{
+			$sql_insert_csv_row = "INSERT INTO $db_table_categories ($csv_fields_str)
+			VALUES ('".implode("', '", $tuple)."')";
+			mysqli_query($db_connection, $sql_insert_csv_row) OR
+				exit ("error inserting `$sql_insert_csv_row` into table: `$db_table_categories`" . mysqli_error($db_connection));
+		}
+	}
 	// $sql_insert_into_test = "INSERT INTO test(id, label) VALUES (?, ?)";
+
 ?>
