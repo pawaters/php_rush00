fe66b445af428ce3e36777cae8e64c4df94669d9
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-17 09:54:20 +0300

    banner img is responsive

--- php/pages/home.php
+++ php/pages/home.php
@@ -1,4 +1,4 @@
 <div class="banner_container">
-    <img src = img/banner_image_Hive.jpeg alt = "Software Engineering">
+    <img class= "banner_img" src = img/banner_image_Hive.jpeg alt = "Software Engineering">
     <div class="centered">OUR SOFTWARE ENGINEERING PORTFOLIO</div>
 </div>
