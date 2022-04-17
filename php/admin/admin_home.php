2a4a0f90d558bca6cfb96fbc4731052d82c458c2
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-17 15:45:39 +0300

    admin style

--- php/admin/admin_home.php
+++ php/admin/admin_home.php
@@ -36,15 +36,13 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Admin</title>
-        <link href="css/home.css" rel="stylesheet">
-        <link href="css/navbar.css" rel="stylesheet">
-        <link href="css/items.css" rel="stylesheet">
-        <link href="css/login.css" rel="stylesheet">
-        <link href="css/cart.css" rel="stylesheet">
-		<link href="css/admin.css" rel="stylesheet">
+        <link href="../../css/home.css" rel="stylesheet">
+        <link href="../../css/navbar.css" rel="stylesheet">
+        <link href="../../css/items.css" rel="stylesheet">
+        <link href="../../css/login.css" rel="stylesheet">
+        <link href="../../css/cart.css" rel="stylesheet">
+		<link href="../../css/admin.css" rel="stylesheet">
     </head>
     <body>
         <nav class="navbar">
