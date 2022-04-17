0505089cd7510cc43cd2a842b0a3f68d5f04ce09
Teemu Hakala <thakala@c1r6p12.hive.fi>
2022-04-17 15:47:16 +0300

    admin_add_item and cart random work

--- php/admin/admin_add_item.php
+++ php/admin/admin_add_item.php
@@ -1,23 +1,54 @@
 <h1>Add an item</h1>
 
-<form>
-	name: <input type="text" name="name" />
+<form action="" method="post">
+	name: <input type="text" name="name" value="<?= @mysqli_real_escape_string($db_connection, $_POST['name']); ?>" />
 	<br>
-	description: <input type="text" name="description" />
+	description: <input type="text" name="description" value="<?= @mysqli_real_escape_string($db_connection, $_POST['description']) ?>" />
 	<br>
-	price: <input type="text" name="price" />
+	price: <input type="number" name="price" value="<?= @mysqli_real_escape_string($db_connection, $_POST['price']) ?>" />
 	<br>
-	image: <input type="text" name="image" />
+	image: <input type="text" name="image" value="<?= @mysqli_real_escape_string($db_connection, $_POST['image']) ?>" />
 	<br>
 	<?php
 		$sql = "SELECT * FROM $db_table_categories";
 		$query = mysqli_query($db_connection, $sql);
-		$arr = mysqli_fetch_assoc($query);
-		var_dump($arr);
-		echo "<input type='checkbox' name='category' value='' /></label>"
+		$arr = mysqli_fetch_all($query);
+		foreach ($arr as $row)
+		{
+			$checked = "";
+			if (in_array($row[0], $_POST["category"]))
+				$checked = "checked";
+			echo "<input type='checkbox' id='$row[1]' name='category[]' value='$row[0]' $checked /><label for='$row[1]'>$row[1]</label>";
+		}
 	?>
+	<input name="submit" type="submit" value="add" />
 </form>
 
 <?php
+	include("../../install.php");
+
+	if (empty($_POST['name']) || empty($_POST['description'])
+		|| empty($_POST['price']) || empty($_POST['image'])
+		|| !isset($_POST["category"]))
+	{
+		exit ("please leave no field blank");
+	}
+	$name = mysqli_real_escape_string($db_connection, $_POST['name']);
+	$price = mysqli_real_escape_string($db_connection, $_POST['price']);
+	$description = mysqli_real_escape_string($db_connection, $_POST['description']);
+	$image = mysqli_real_escape_string($db_connection, $_POST['image']);
+	$sql = "INSERT INTO $db_table_items (`name`, `price`, `description`, `img`)
+		VALUES ('$name', '$price', '$description', '$image')";
+	mysqli_query($db_connection, $sql);
+
+	$query = mysqli_query($db_connection, "SELECT LAST_INSERT_ID()") OR
+		exit("error querying database" . mysqli_error($db_connection));
+	$item_id = mysqli_fetch_assoc($query)["LAST_INSERT_ID()"];
+	foreach ($_POST["category"] as $category)
+	{
+		$sql = "INSERT INTO $db_table_item_categories (id_item, id_category)
+			VALUES ($item_id, $category)";
+		mysqli_query($db_connection, $sql);
+	}
 
 ?>
